50c05bd826a8d5fa9d5788b74b8287f58fce49cf
Aditya Kumar <aditya97jan@gmail.com>
2020-05-14 22:46:04 -0400

    Create TwoSum.py

---
+++ Array/TwoSum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic={}
+        for i,val in enumerate(nums):
+            if val not in dic:
+                dic[target-val]=i
+            else:
+                return {i,dic[val]}
